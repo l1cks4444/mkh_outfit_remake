502a87b03d9bba5b1774bf88d9e821c37333bac2
L1cKs #444 <138713607+l1cks4444@users.noreply.github.com>
2025-09-18 15:42:43 +0200

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,19 @@
+Config = {}
+
+Config.Webhook = 'WEBHOOK BRUDER'
+Config.AllowedGroups = { 'pl', 'mod' }
+Config.AllowSelfChange = false
+
+Config.Language = {
+    no_permission = {text = 'Keine Rechte!', type = 'error'},
+    invalid_id = {text = 'Ungültige Spieler ID!', type = 'error'},
+    self_select = {text = 'Du kannst dich nicht Selber Changen!', type = 'warning'},
+    player_not_found = {text = 'Spieler nicht gefunden oder offline!', type = 'error'},
+    menu_opened = {text = 'Menü für Spieler %s geöffnet.', type = 'success'}
+}
+
+Config.Discord = {
+    username = 'Outfit Change Log',
+    color = 16753920,
+    title = 'Force Change geöffnet',
+}
